af90fd77680e14bb6c0aeabac9d36ac326e9aeab
Sai Krishna <saikrishna321@yahoo.com>
2015-11-10 07:15:31 +0530

    added junit report formatting

--- src/test/java/com/paralle/tests/Runner.java
+++ src/test/java/com/paralle/tests/Runner.java
@@ -1,10 +1,17 @@
 package com.paralle.tests;
 
+import org.testng.annotations.Test;
+
 import com.appium.manager.ParallelThread;
 
 public class Runner {
 
-	public static void main(String[] args) throws Exception {
+//	public static void main(String[] args) throws Exception {
+//		
+//	}
+	
+	@Test
+	public void testRunner() throws Exception{
 		ParallelThread parallelThread = new ParallelThread();
 		parallelThread.runner("com.paralle.tests");
 	}
